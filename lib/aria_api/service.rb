f6d7f4ba4707c83672b91e639953d5516b13a4fe
Li Chen <kyrrechen@gmail.com>
2013-08-31 06:34:04 +0800

    use pre-downloaded wsdl to speed up testing

--- lib/aria_api/service.rb
+++ lib/aria_api/service.rb
@@ -2,7 +2,9 @@ module AriaApi
   class Service
     def self.actions
       api_version = AriaApi::Configuration.api_version
-      url = "https://secure.future.stage.ariasystems.net/api/Advanced/wsdl/#{api_version}/complete-doc_literal_wrapped.wsdl"
+      wsdl_name = "complete-doc_literal_wrapped.wsdl"
+      #url = "https://secure.future.stage.ariasystems.net/api/Advanced/wsdl/#{api_version}/#{wsdl_name}"
+      url = "#{File.dirname(__FILE__)}/wsdl/#{wsdl_name}"
       client = Savon.client(wsdl: url)
       @actions ||= client.operations
     end
--- lib/aria_api/version.rb
+++ lib/aria_api/version.rb
@@ -1,3 +1,3 @@
 module AriaApi
-  VERSION = "0.0.2.1"
+  VERSION = "0.6.12"
 end
